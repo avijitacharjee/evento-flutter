d1567b3e41ca430fb8b5ef4808e235e6d52f6d9b
AvijitAcharjee <avijitach@gmail.com>
2024-01-31 15:42:18 +0600

    home page

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,6 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -38,7 +37,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-
       _counter++;
     });
   }
@@ -64,7 +62,10 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Row(
           children: [
-            Text(widget.title,style: TextStyle(color: Colors.white),),
+            Text(
+              widget.title,
+              style: TextStyle(color: Colors.white),
+            ),
           ],
         ),
         iconTheme: IconThemeData(color: Colors.white),
@@ -76,7 +77,8 @@ class _MyHomePageState extends State<MyHomePage> {
             Container(
               decoration: const BoxDecoration(
                 color: Colors.white,
-                borderRadius: BorderRadius.only(bottomRight: Radius.circular(20)),
+                borderRadius:
+                    BorderRadius.only(bottomRight: Radius.circular(20)),
               ),
               child: Padding(
                 padding: const EdgeInsets.fromLTRB(20, 40, 0, 20),
@@ -85,14 +87,15 @@ class _MyHomePageState extends State<MyHomePage> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Image.asset('assets/images/evento.png'),
-                    const SizedBox(height: 20,),
+                    const SizedBox(
+                      height: 20,
+                    ),
                     const Text(
                       'Avijit Acharjee',
                       style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold
-                      ),
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
                       textAlign: TextAlign.left,
                     ),
                     const Text(
@@ -129,7 +132,6 @@ class _MyHomePageState extends State<MyHomePage> {
                 });
               },
             ),
-
             ListTile(
               leading: const Icon(Icons.account_circle),
               title: const Text('Profile'),
@@ -152,7 +154,9 @@ class _MyHomePageState extends State<MyHomePage> {
               leading: const Icon(Icons.share),
               title: const Text("Share"),
             ),
-            const Divider(height: 1,),
+            const Divider(
+              height: 1,
+            ),
             const ListTile(
               leading: Icon(Icons.logout),
               title: const Text('Log out'),
@@ -160,19 +164,235 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      body: Center(
-        child: Container(
-          width: 100,
-          height: 100,
-          color: Colors.indigoAccent,
-          child: const Text(
-              "Hello Avijit..",
-            style: TextStyle(color: Colors.white),
-            textAlign: TextAlign.center,
+      body: Expanded(
+        child: SingleChildScrollView(
+          child: Container(
+            width: double.infinity,
+            color: const Color(0xff111315),
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsetsDirectional.all(20),
+                  height: 300,
+                  color: const Color(0xff272a2f),
+                  child: Column(
+                    children: [
+                      const Text(
+                        "The Easiest And Most Powerful Online Event Booking and Ticketing System.",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Text(
+                        "Evento is an all-in-one event ticketing platform for event organizers, promoters, and managers. Easily create,promote and manage your events of any type and size",
+                        style: TextStyle(color: Colors.white60, fontSize: 16),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        width: 200,
+                        height: 50,
+                        color: const Color(0xff6ac045),
+                        child: const Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Create Event",
+                              style: TextStyle(color: Colors.white, fontSize: 18),
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Icon(
+                              Icons.arrow_circle_right,
+                              color: Colors.white,
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  "Explore Events",
+                  style: TextStyle(color: Colors.white, fontSize: 22),
+                ),
+                Column(
+                  children: [
+                    Card(
+                      margin: EdgeInsets.all(20),
+                      color: Color(0xff1a1d1f),
+                      child: Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Image.network(
+                                "https://fastly.picsum.photos/id/195/640/360.jpg?hmac=OYBzVUKVbPiHulCq_7wGwdGPr3aJUMTyFi019ptgWzQ"),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              "   PUC Convocation",
+                              style: TextStyle(color: Colors.white, fontSize: 16),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Text(
+                              "   BDT 200",
+                              style: TextStyle(color: Colors.white60, fontSize: 16),
+                            ),
+                            // Container(
+                            //   // color: Color.fromRGBO(106, 192, 69, 3),
+                            //   child: Row(
+                            //     mainAxisAlignment: MainAxisAlignment.start,
+                            //     children: [
+                            //       Icon(Icons.calendar_today,color: Colors.white60,),
+                            //       Text("12 Sep")
+                            //     ],
+                            //   ),
+                            // )
+                            SizedBox(
+                              height: 20,
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      margin: EdgeInsets.all(20),
+                      color: Color(0xff1a1d1f),
+                      child: Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Image.network(
+                                "https://fastly.picsum.photos/id/678/640/360.jpg?hmac=DVM3Z2BtIVOUL6Y3oPKrk9ev7B1tHTYEb5xYRhlGAJU"),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              "   PUC Convocation",
+                              style: TextStyle(color: Colors.white, fontSize: 16),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Text(
+                              "   BDT 200",
+                              style: TextStyle(color: Colors.white60, fontSize: 16),
+                            ),
+                            // Container(
+                            //   // color: Color.fromRGBO(106, 192, 69, 3),
+                            //   child: Row(
+                            //     mainAxisAlignment: MainAxisAlignment.start,
+                            //     children: [
+                            //       Icon(Icons.calendar_today,color: Colors.white60,),
+                            //       Text("12 Sep")
+                            //     ],
+                            //   ),
+                            // )
+                            SizedBox(
+                              height: 20,
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      margin: EdgeInsets.all(20),
+                      color: Color(0xff1a1d1f),
+                      child: Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Image.network(
+                                "https://fastly.picsum.photos/id/195/640/360.jpg?hmac=OYBzVUKVbPiHulCq_7wGwdGPr3aJUMTyFi019ptgWzQ"),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              "   PUC Convocation",
+                              style: TextStyle(color: Colors.white, fontSize: 16),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Text(
+                              "   BDT 200",
+                              style: TextStyle(color: Colors.white60, fontSize: 16),
+                            ),
+                            // Container(
+                            //   // color: Color.fromRGBO(106, 192, 69, 3),
+                            //   child: Row(
+                            //     mainAxisAlignment: MainAxisAlignment.start,
+                            //     children: [
+                            //       Icon(Icons.calendar_today,color: Colors.white60,),
+                            //       Text("12 Sep")
+                            //     ],
+                            //   ),
+                            // )
+                            SizedBox(
+                              height: 20,
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      margin: EdgeInsets.all(20),
+                      color: Color(0xff1a1d1f),
+                      child: Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Image.network(
+                                "https://fastly.picsum.photos/id/195/640/360.jpg?hmac=OYBzVUKVbPiHulCq_7wGwdGPr3aJUMTyFi019ptgWzQ"),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              "   PUC Convocation",
+                              style: TextStyle(color: Colors.white, fontSize: 16),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Text(
+                              "   BDT 200",
+                              style: TextStyle(color: Colors.white60, fontSize: 16),
+                            ),
+                            // Container(
+                            //   // color: Color.fromRGBO(106, 192, 69, 3),
+                            //   child: Row(
+                            //     mainAxisAlignment: MainAxisAlignment.start,
+                            //     children: [
+                            //       Icon(Icons.calendar_today,color: Colors.white60,),
+                            //       Text("12 Sep")
+                            //     ],
+                            //   ),
+                            // )
+                            SizedBox(
+                              height: 20,
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
-      ),
+      )
     );
   }
 }
-
